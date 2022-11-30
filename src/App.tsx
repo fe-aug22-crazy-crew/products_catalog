9bb1bea3e7851aa5f1c7add731e4b541de067f86
Maksym845 <mzuravel845@gmail.com>
2022-11-30 10:53:18 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello everyone from Crazy Crew!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header className={'header'}>
+      <p>
+        Hello, new component!
+      </p>
+    </header>
+  );
+};
