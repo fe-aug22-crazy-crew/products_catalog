47d86e537f50fb89680bc4beb1176b2d8e4aaff7
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-11-30 22:27:40 +0200

    deleted unnecessary import

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { LinkFooter, } from './LinkFooter/index';
+import { LinkFooter } from './LinkFooter/index';
 import logo from '../../images/logo.png';
 
 const navFields = [
