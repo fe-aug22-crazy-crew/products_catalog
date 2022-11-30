38476efa40faa165e6ce3b5ce7e93af48f4d3a4d
Artem Musii <musiy.artem@gmail.com>
2022-11-30 20:37:56 +0200

    card layout added

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,7 +5,5 @@ import { PhoneCard } from '../PhoneCard/PhoneCard';
   /* Homepage content here */
 }
 export const HomePage: React.FC = () => {
-  return (
-    <PhoneCard />
-  );
+  return <PhoneCard />;
 };
--- src/request.ts
+++ src/request.ts
@@ -1,8 +1,9 @@
 import { Phone } from './types/Phone';
 
-const BASE_URL = 'https://teal-tiramisu-13c82d.netlify.app/.netlify/functions/server/';
+const BASE_URL =
+  'https://teal-tiramisu-13c82d.netlify.app/.netlify/functions/server/';
 
-export const getAllPhones = async() => {
+export const getAllPhones = async () => {
   const response = await fetch(BASE_URL + 'phones');
   const phones: Phone[] = await response.json();
 
