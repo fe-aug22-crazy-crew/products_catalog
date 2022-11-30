6437cc6ec922f1637a5427a90e6dc1aa9edd821a
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-11-30 21:41:51 +0200

    added styles for footer and the fonts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { NavLinkFooter} from './NavLinkFooter/index';
-import logo from '../../images/Logo.png';
-import arrow from '../../images/Arrow.svg';
+import { NavLinkFooter } from './NavLinkFooter/index';
+import logo from '../../images/logo.png';
 
 const navFields = [
-  {title: 'Github', to: 'https://github.com/fe-aug22-crazy-crew/products_catalog', id: 1 },
-  {title: 'Contacts', to: '/index.html', id: 2 },
-  {title: 'Rights', to: '/index.html', id: 3 },
+  {
+    title: 'Github',
+    to: 'https://github.com/fe-aug22-crazy-crew/products_catalog',
+    id: 1,
+  },
+  { title: 'Contacts', to: '/index.html', id: 2 },
+  { title: 'Rights', to: '/index.html', id: 3 },
 ];
 
 export const Footer: React.FC = React.memo(() => (
@@ -16,38 +19,21 @@ export const Footer: React.FC = React.memo(() => (
     <div className="container">
       <div className="footer__content">
         <Link to="/index.html" className="logo footer__logo">
-          <img
-            src={logo}
-            alt="logo Nice Gadgetes"
-            className="logo__img"
-          />
+          <img src={logo} alt="logo Nice Gadgetes" className="logo__img" />
         </Link>
 
         <nav className="footer__nav">
           <ul className="footer__list">
-            {navFields.map(({ 
-              title,
-              to,
-              id
-            }) => (
-              <NavLinkFooter 
-                key={id} 
-                title={title} 
-                to={to} />
+            {navFields.map(({ title, to, id }) => (
+              <NavLinkFooter key={id} title={title} to={to} />
             ))}
           </ul>
         </nav>
 
-        <div className="footer__extra">
+        <Link to="#home" className="footer__button--up">
           <p className="footer__back-to-top">Back to top</p>
-          <Link to="#" className="footer__arrow">
-            <img
-              src={arrow}
-              alt="logo Nice Gadgetes"
-              className="footer__arrow-img"
-            />
-          </Link>
-        </div>
+          <div className="footer__arrow"></div>
+        </Link>
       </div>
     </div>
   </footer>
