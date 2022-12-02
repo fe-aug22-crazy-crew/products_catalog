b28ad5b0e1946c416aaca69b6e390f2da5cb796b
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-12-02 20:54:41 +0200

    created file with constants for the footer component

---
+++ src/components/Footer/constants.ts
@@ -0,0 +1,9 @@
+export const infoFields = [
+  {
+    title: 'Github',
+    to: 'https://github.com/fe-aug22-crazy-crew/products_catalog',
+    id: 1,
+  },
+  { title: 'Contacts', to: '/index.html', id: 2 },
+  { title: 'Rights', to: '/index.html', id: 3 },
+];
