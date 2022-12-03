e1641978e8e3343b424e1047646fa30a2f7baae3
Maksym Zhuravel <100576482+Maksym845@users.noreply.github.com>
2022-12-03 23:55:41 +0200

    Burger menu update (#24)
    * brand new models slider was added, some code was refactored
    
    * brand new models slider was added, some code was refactored
    
    * Burger menu was updated
    
    * Delete .idea directory

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 
 import { useAppSelector } from '../../app/hooks';
-import { Slider } from '../../components/Slider';
+import { Slider } from '../Slider';
 import { Promo } from './Promo';
 
 import './homePage.scss';
-import './HomePage.scss';
+import { SliderButtons } from './SliderButtons';
 
 export const HomePage: React.FC = () => {
   const phones = useAppSelector((state) => state.phones);
 
   return (
-    <>
-      <main className="home_page">
-        <h1 className="home_page__title">Welcome to Nice Gadgets store!</h1>
-        <Promo />
-      </main>
-      <main className="homepage">
-        <div className="container">
-          <div className="homepage__title-box">
-            <h2 className="homepage__subtitle">Hot prices</h2>
-            <div className="homepage__button-box">
-              <div className="homepage__button-prev"></div>
-              <div className="homepage__button-next"></div>
-            </div>
-          </div>
-          <Slider phones={phones} />
-        </div>
-      </main>
-    </>
+    <main className="home_page">
+      <h1 className="home_page__title home_page__title--h1">
+        Welcome to Nice Gadgets store!
+      </h1>
+      <Promo />
+
+      <div className="home_page__title-box">
+        <h2 className="home_page__subtitle">Brand new models</h2>
+        <SliderButtons
+          refClassPrev={'newest_button_prev'}
+          refClassNext={'newest_button_next'}
+        />
+      </div>
+      <Slider
+        phones={phones}
+        prevButtonClass={'.newest_button_prev'}
+        nextButtonClass={'.newest_button_next'}
+      />
+
+      <div className="home_page__title-box">
+        <h2 className="home_page__subtitle">Hot prices</h2>
+        <SliderButtons
+          refClassPrev={'hot_button_prev'}
+          refClassNext={'hot_button_next'}
+        />
+      </div>
+      <Slider
+        phones={phones}
+        prevButtonClass={'.hot_button_prev'}
+        nextButtonClass={'.hot_button_next'}
+      />
+    </main>
   );
 };
