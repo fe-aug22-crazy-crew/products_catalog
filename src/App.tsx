7d214e9e01211e7f38f8ff699be939de3f7dc245
Artem Nasadchuk <nasadchuk007@gmail.com>
2022-12-03 02:22:41 +0200

    hot price slider

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 
       {/* Only content will change here */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="phones" element={<Phones />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAppSelector } from '../../app/hooks';
-import { Phone } from '../../types/Phone';
+//import { Phone } from '../../types/Phone';
+import { Slider } from '../../components/Slider';
+import './HomePage.scss';
 {
   /* Homepage content here */
 }
@@ -9,5 +11,20 @@ export const HomePage: React.FC = () => {
     /* Use this syntax to get data from redux provider*/
   }
   const phones = useAppSelector((state) => state.phones);
-  return phones.map((phone: Phone) => <h3 key={phone.id}>{phone.phoneId}</h3>);
+  //return phones.map((phone: Phone) => <h3 key={phone.id}>{phone.phoneId}</h3>);
+
+  return (
+    <main className="homepage">
+      <div className="container">
+        <div className="homepage__title-box">
+          <h2 className="homepage__subtitle">Hot prices</h2>
+          <div className="homepage__button-box">
+            <div className="homepage__button-prev"></div>
+            <div className="homepage__button-next"></div>
+          </div>
+        </div>
+        <Slider phones={phones} />
+      </div>
+    </main>
+  );
 };
