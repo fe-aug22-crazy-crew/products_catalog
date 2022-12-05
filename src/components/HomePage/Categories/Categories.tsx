e149486e81a0ab7a63bbe39a0b21dcbef0c92a9f
Maksym845 <mzuravel845@gmail.com>
2022-12-05 17:09:12 +0200

    categories block was changed

--- src/components/HomePage/Categories/Categories.tsx
+++ src/components/HomePage/Categories/Categories.tsx
@@ -13,30 +13,21 @@ export const Categories: React.FC = () => {
       <h2 className="categories__title">Shop by category</h2>
       <div className="categories__main">
         <div className="categories__category">
-          <NavLink
-            to={'/phones'}
-            className="categories__image-block"
-          >
+          <NavLink to={'/phones'} className="categories__image-block">
             <img src={Phones} alt="category" className="categories__image" />
           </NavLink>
           <h4>Mobile Phones</h4>
           <p className="categories__count">{'95 models'}</p>
         </div>
         <div className="categories__category">
-          <NavLink
-            to={'/tablets'}
-            className="categories__image-block"
-          >
+          <NavLink to={'/tablets'} className="categories__image-block">
             <img src={Tablets} alt="category" className="categories__image" />
           </NavLink>
           <h4>Tablets</h4>
           <p className="categories__count">{'70 models'}</p>
         </div>
         <div className="categories__category">
-          <NavLink
-            to={'/accessories'}
-            className="categories__image-block"
-          >
+          <NavLink to={'/accessories'} className="categories__image-block">
             <img
               src={Accessories}
               alt="category"
