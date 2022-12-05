9d0f5ec37ec149985e63ec2074bfa8cbf245f67b
Anastasiia Nahaiska <110456763+anastasiia-nahaiska@users.noreply.github.com>
2022-12-05 16:35:27 +0300

    fixed modal width for phone (#30)

--- src/components/HomePage/Categories/Categories.tsx
+++ src/components/HomePage/Categories/Categories.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import Phones from '../../../images/categories/phones.png';
-import Tablets from '../../../images/categories/tablets.png';
-import Accessories from '../../../images/categories/acs.png';
+// import Phones from '../../../images/categories/phones.png';
+// import Tablets from '../../../images/categories/tablets.png';
+// import Accessories from '../../../images/categories/acs.png';
 
 import './categories.scss';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export const Categories: React.FC = () => {
   return (
     <section className="categories">
       <h2 className="categories__title">Shop by category</h2>
       <div className="categories__main">
-        <div className="categories__category">
+        {/* <div className="categories__category">
           <Link
             to={'/phones'}
             className="categories__image-block categories__image-block--phones"
@@ -46,7 +46,7 @@ export const Categories: React.FC = () => {
           </Link>
           <h4>Accessories</h4>
           <p className="categories__count">{'115 models'}</p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
