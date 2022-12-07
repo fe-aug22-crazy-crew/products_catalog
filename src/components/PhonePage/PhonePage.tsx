6a5f77d2415c81f64911f42beac4df2d8b9d9809
Artem Nasadchuk <nasadchuk007@gmail.com>
2022-12-07 21:34:07 +0200

    test request

--- src/components/PhonePage/PhonePage.tsx
+++ src/components/PhonePage/PhonePage.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Slider } from '../Slider';
 import { Info } from './Info/Info';
 
+import { client } from '../../utils/fetchPhones';
+
 export const PhonePage: React.FC = () => {
+  const [recPhones, setRecPhones] = useState([]);
+
+  const getRecPhones = async() => {
+    const data = await client.get(
+      'phones/'
+      + location.pathname.split('/').slice(-1).join('') + '/recommended',
+      'GET',
+      null,
+    );
+
+    setRecPhones(data);
+  };
+
+  useEffect(() => {
+    getRecPhones();
+  }, []);
+
   return (
     <main>
       <div className="container">
         <Info />
+        <Slider
+          phones={recPhones}
+          title={'You may also like'}
+        />
       </div>
     </main>
   );
