9feb51b2085271eb1490a3f553815ecbea2418a1
Maksym845 <mzuravel845@gmail.com>
2022-12-09 18:42:14 +0200

    small size fix phone page

--- src/components/PhonePage/PhonePageMain/PhonePageMain.tsx
+++ src/components/PhonePage/PhonePageMain/PhonePageMain.tsx
@@ -10,6 +10,7 @@ import { FavouriteIcon } from '../../PhoneCard/FavouriteIcon';
 import notFavourite from '../../../images/favourites.svg';
 import favourite from '../../../images/selectedFavourite.svg';
 import { PhoneData } from '../../../types/PhoneData';
+import { Colors } from '../../../utils/phoneColors';
 
 import { useDispatch } from 'react-redux';
 import { Phone } from '../../../types/Phone';
@@ -86,22 +87,6 @@ export const PhonePageMain: React.FC<Props> = ({ phone, setUpdate }) => {
   useEffect(() => {
     window.localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
-
-  const Colors = {
-    white: '#F0F0F0',
-    yellow: '#F3D060',
-    red: '#A5282C',
-    coral: '#EE7762',
-    blue: '#5EB0E5',
-    purple: '#D1CDDA',
-    black: '#1F2020',
-    green: '#AEE1CD',
-    rosegold: '#E6C7C2',
-    spacegray: '#25282A',
-    midnightgreen: '#004953',
-    gold: '#f0ae04',
-    silver: '#E4E4E2',
-  };
 
   const handleChangeColor = (color: string) => {
     const currentPath = document.location.hash.slice(1).split('-');
