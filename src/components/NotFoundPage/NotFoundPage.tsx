82aa9dec36abb9eda0b4a03c7c4ed7d7c4c65039
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-12-11 13:52:03 +0200

    added try catch to the requests on phone page

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export const NotFoundPage: React.FC = () => (
   <main className="notFoundPage">
     <h1 className="notFoundPage__title">Opss... You got lost &#x1F625;</h1>
-    <Link to="products_catalog" className="notFoundPage__button">
+    <Link to="/" className="notFoundPage__button">
       Back to Home
     </Link>
   </main>
